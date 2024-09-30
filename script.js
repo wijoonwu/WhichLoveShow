5221a9ed94e88eca004c3a0b766b543c3508abf4
wijoonwu <devjoon@nate.com>
2024-09-30 17:27:54 +0900

    Update script.js

--- script.js
+++ script.js
@@ -3,90 +3,90 @@ const questions = [
     text: "애인이 내 사진을 SNS에 올리려고 하는 경우가 많았다.",
     program: "하트시그널",
   },
-  {
-    text: "나는 사람들의 관심이 부담스럽지 않고, 오히려 즐긴다.",
-    program: "솔로 지옥",
-    alternativeProgram: "나는 솔로",
-  },
-  {
-    text: "나는 모르는 사람에게 인스타그램으로 DM을 자주 받는다.",
-    program: "솔로 지옥",
-    alternativeProgram: "나는 솔로",
-  },
-  {
-    text: "먼저 나서지 않아도 다가와주는 사람들이 많은 편이다.",
-    program: "하트시그널",
-    alternativeProgram: "나는 솔로",
-  },
-  {
-    text: "마음에 드는 사람이 생기면 어떻게 다가갈지 안다.",
-    program: "솔로 지옥",
-    alternativeProgram: "나는 솔로",
-  },
-  {
-    text: "나는 내 직업을 사랑하고 열정적으로 임한다.",
-    program: "하트시그널",
-    multiplePrograms: ["하트시그널", "솔로 지옥", "나는 솔로"],
-  },
-  {
-    text: "소개팅 제의가 자주 들어오는 편이다.",
-    program: "하트시그널",
-  },
-  {
-    text: "나는 나를 좋아하는 사람보다 내가 좋아하는 사람이 좋다.",
-    program: "솔로 지옥",
-    alternativeProgram: "하트시그널",
-  },
-  {
-    text: "꾸준히 하는 운동이 있다.",
-    program: "솔로 지옥",
-    multiplePrograms: ["하트시그널", "솔로 지옥"],
-    alternativeProgram: "나는 솔로",
-  },
-  {
-    text: "굳이 애인이 있는 것을 SNS에 티내고 싶지 않다.",
-    program: "솔로 지옥",
-  },
-  {
-    text: "가는 사람 안잡고 오는 사람 안막는다.",
-    program: "솔로 지옥",
-  },
-  {
-    text: "나는 질투 혹은 집착이 있는 편이다.",
-    program: "환승연애",
-  },
-  {
-    text: "애인에게 자주 속상하고 서운한 감정을 느낀다.",
-    program: "환승연애",
-  },
-  {
-    text: "사람들 앞에서 나서게 되면 긴장되고 어색하다.",
-    program: "나는 솔로",
-    alternativeProgram: "솔로 지옥",
-  },
-  {
-    text: "나는 굳이 타인에게 먼저 다가가지 않고, 오는 사람도 경계가 된다.",
-    program: "나는 솔로",
-  },
-  {
-    text: "낯선 사람과 이야기하는 것이 어색하지 않다.",
-    program: "솔로 지옥",
-    multiplePrograms: ["하트시그널", "솔로 지옥"],
-  },
-  {
-    text: "보통 자연스러운 만남을 통해 인연을 가지는 편이다.",
-    program: "하트시그널",
-    multiplePrograms: ["하트시그널", "환승연애", "나는 솔로"],
-  },
-  {
-    text: "헤어지고 나서 새로운 연애를 시작하기까지 텀이 짧은 편이다.",
-    program: "솔로 지옥",
-    alternativeProgram: "환승연애",
-  },
-  {
-    text: "장기간 사귄 전 애인이 있다.",
-    program: "환승연애",
-  },
+  //   {
+  //     text: "나는 사람들의 관심이 부담스럽지 않고, 오히려 즐긴다.",
+  //     program: "솔로 지옥",
+  //     alternativeProgram: "나는 솔로",
+  //   },
+  //   {
+  //     text: "나는 모르는 사람에게 인스타그램으로 DM을 자주 받는다.",
+  //     program: "솔로 지옥",
+  //     alternativeProgram: "나는 솔로",
+  //   },
+  //   {
+  //     text: "먼저 나서지 않아도 다가와주는 사람들이 많은 편이다.",
+  //     program: "하트시그널",
+  //     alternativeProgram: "나는 솔로",
+  //   },
+  //   {
+  //     text: "마음에 드는 사람이 생기면 어떻게 다가갈지 안다.",
+  //     program: "솔로 지옥",
+  //     alternativeProgram: "나는 솔로",
+  //   },
+  //   {
+  //     text: "나는 내 직업을 사랑하고 열정적으로 임한다.",
+  //     program: "하트시그널",
+  //     multiplePrograms: ["하트시그널", "솔로 지옥", "나는 솔로"],
+  //   },
+  //   {
+  //     text: "소개팅 제의가 자주 들어오는 편이다.",
+  //     program: "하트시그널",
+  //   },
+  //   {
+  //     text: "나는 나를 좋아하는 사람보다 내가 좋아하는 사람이 좋다.",
+  //     program: "솔로 지옥",
+  //     alternativeProgram: "하트시그널",
+  //   },
+  //   {
+  //     text: "꾸준히 하는 운동이 있다.",
+  //     program: "솔로 지옥",
+  //     multiplePrograms: ["하트시그널", "솔로 지옥"],
+  //     alternativeProgram: "나는 솔로",
+  //   },
+  //   {
+  //     text: "굳이 애인이 있는 것을 SNS에 티내고 싶지 않다.",
+  //     program: "솔로 지옥",
+  //   },
+  //   {
+  //     text: "가는 사람 안잡고 오는 사람 안막는다.",
+  //     program: "솔로 지옥",
+  //   },
+  //   {
+  //     text: "나는 질투 혹은 집착이 있는 편이다.",
+  //     program: "환승연애",
+  //   },
+  //   {
+  //     text: "애인에게 자주 속상하고 서운한 감정을 느낀다.",
+  //     program: "환승연애",
+  //   },
+  //   {
+  //     text: "사람들 앞에서 나서게 되면 긴장되고 어색하다.",
+  //     program: "나는 솔로",
+  //     alternativeProgram: "솔로 지옥",
+  //   },
+  //   {
+  //     text: "나는 굳이 타인에게 먼저 다가가지 않고, 오는 사람도 경계가 된다.",
+  //     program: "나는 솔로",
+  //   },
+  //   {
+  //     text: "낯선 사람과 이야기하는 것이 어색하지 않다.",
+  //     program: "솔로 지옥",
+  //     multiplePrograms: ["하트시그널", "솔로 지옥"],
+  //   },
+  //   {
+  //     text: "보통 자연스러운 만남을 통해 인연을 가지는 편이다.",
+  //     program: "하트시그널",
+  //     multiplePrograms: ["하트시그널", "환승연애", "나는 솔로"],
+  //   },
+  //   {
+  //     text: "헤어지고 나서 새로운 연애를 시작하기까지 텀이 짧은 편이다.",
+  //     program: "솔로 지옥",
+  //     alternativeProgram: "환승연애",
+  //   },
+  //   {
+  //     text: "장기간 사귄 전 애인이 있다.",
+  //     program: "환승연애",
+  //   },
 ];
 
 let scores = {
